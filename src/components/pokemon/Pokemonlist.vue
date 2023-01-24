1715c20acedf33d6e656ec16dd7c2a1f8f1dc8a0
antonionovelli92 <anto.novelli@hotmail.it>
2023-01-24 19:22:52 +0100

    change img

--- src/components/pokemon/Pokemonlist.vue
+++ src/components/pokemon/Pokemonlist.vue
@@ -10,7 +10,7 @@ export default {
         }
     },
     created() {
-        axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=1')
+        axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=4')
             .then(res => {
                 this.pokemon = res.data.docs;
             })
