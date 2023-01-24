35b1850d683dbb3d556c1503aa74d5d84c914653
antonionovelli92 <anto.novelli@hotmail.it>
2023-01-24 17:42:48 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <script>
-
+import AppHeader from './components/AppHeader.vue';
 export default {
-
+    components: { AppHeader }
 }
 </script>
 
 <template>
-    <header>
-        <div class="d-flex text-center justify-content-center align-items-center mt-4 p-1">
-            <v-icon PiCharmander="bi-music-player" />
-            <v-icon name="" />
-            <v-icon name="pi-bulbasaur" animation="pulse" scale="4" />
-            <v-icon name="pi-charmander" animation="pulse" scale="4" />
-            <v-icon name="pi-squirtle" animation="pulse" scale="4" />
-
-        </div>
-    </header>
+    <app-header></app-header>
 </template>
 
 <style lang="scss">
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,20 @@
 <script>
 export default {
+    name: 'AppHeader',
 
 }
 </script>
 
 <template>
+    <header>
+        <div class="d-flex text-center justify-content-center align-items-center mt-4 p-1">
+            <v-icon PiCharmander="bi-music-player" />
+            <v-icon name="" />
+            <v-icon name="pi-bulbasaur" animation="pulse" scale="4" />
+            <v-icon name="pi-charmander" animation="pulse" scale="4" />
+            <v-icon name="pi-squirtle" animation="pulse" scale="4" />
+        </div>
+    </header>
 
 </template>
 
